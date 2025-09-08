faaa92d8adfe5879e768a08fec1bb8a45cf8d552
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-09-08 16:16:26 +0700

    hapus secret

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   baseUrl: 'https://backend.nacademy.my.id/api',
   recaptchaSiteKey: '6Lfv8MErAAAAAAAw32NIV_zvuWhQ6zLM0n7C5mKu',
-  recaptchaSecret: '6Lfv8MErAAAAAEmqoRhSsNH4_nwbM4BfFza3BCzv',
 };
